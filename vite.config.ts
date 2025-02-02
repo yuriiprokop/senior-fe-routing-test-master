a461789f63e7c7a37a0b8387006550a6d197af96
yura prokopchuk <46374597+yuriiprokop@users.noreply.github.com>
2025-02-02 14:26:40 +0200

    [FEAT][FRM] library build (#4)

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,27 @@
 /// <reference types="vitest" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: 'SeniorFeRoutingTest',
+      fileName: (format) => `senior-fe-routing-test.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  },
   test: {
     globals: true,
     environment: 'jsdom',
@@ -14,4 +30,3 @@ export default defineConfig({
     setupFiles: './setup-tests.ts',
   },
 });
- 
