b4ef1574faf99f1e38dc501043c2b7cd3f68c576
yura prokopchuk <46374597+yuriiprokop@users.noreply.github.com>
2025-02-02 14:25:05 +0200

    [FEAT][FRM] library build

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export { default as Link } from './components/Link/Link';
+export { default as Router } from './components/Router/Router';
+export { default as useParams } from './hooks/use-params/use-params';
+export type { RouterConfig } from './types/router.type';
